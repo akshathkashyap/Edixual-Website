d4bf94b281c9122dc50dbe77d0ec61a0eae391c3
akshathkashyap <akshath.kp99@gmail.com>
2021-09-27 17:38:22 +0530

    second final commit

--- src/components/EXCOriginalsDirector.js
+++ src/components/EXCOriginalsDirector.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react'
 
 import './EXCOriginalsDirector.css'
@@ -63,69 +62,3 @@ function EXCOriginalsDirector() {
 }
 
 export default EXCOriginalsDirector
-=======
-import React, { useState, useEffect } from 'react'
-
-import './EXCOriginalsDirector.css'
-
-function EXCOriginalsDirector() {
-  const handleMouseOver = (img, secTitle, p1, p2) => {
-    img.style.filter = 'grayscale(0)'
-    secTitle.style.color = 'var(--primary-text-color)'
-    p1.style.cssText = 'transform: translateX(0); opacity: 1'
-    p2.style.cssText = 'transform: translateX(0); opacity: 1'
-  }
-  const handleMouseOut = (img, secTitle) => {
-    secTitle.style.color = 'var(--secondary-text-color)'
-  }
-  const scrollHandler = () => {
-    var img = document.querySelector('.img-wrapper img')
-    var secTitle = document.querySelector('.director .section-title')
-    var p1 = document.querySelector('.director-img-container p')
-    var p2 = document.querySelector('.director-about-container p')
-    var director = document.querySelector('.director .section-title')
-    var directorTop = director.getBoundingClientRect().top
-    if (directorTop <= (window.innerHeight / 3)) {
-      handleMouseOver(img, secTitle, p1, p2)
-      document.removeEventListener('scroll', scrollHandler)
-    }
-  }
-  useEffect(() => {
-    var img = document.querySelector('.img-wrapper img')
-    var secTitle = document.querySelector('.director .section-title')
-    var p1 = document.querySelector('.director-img-container p')
-    var p2 = document.querySelector('.director-about-container p')
-
-    img.addEventListener('mouseover', () => handleMouseOver(img, secTitle, p1, p2))
-    img.addEventListener('mouseout', () => handleMouseOut(img, secTitle))
-
-    return () => {
-      img.removeEventListener('mouseover', handleMouseOver)
-      img.removeEventListener('mouseout', handleMouseOut)
-    }
-  })
-  useEffect(() => {
-    document.addEventListener('scroll', scrollHandler)
-    return () => {
-      document.removeEventListener('scroll', scrollHandler)
-    }
-  })
-
-  return (
-    <div className="director section-container">
-      <h1 className="section-title">director</h1>
-      <div className="director-img-container">
-        <p>aniket sritava	&#8594;</p>
-        <div className="img-wrapper">
-          <img src={ process.env.PUBLIC_URL + '/images/director.jpg' } alt="Aniket Sritava" />
-        </div>
-      </div>
-      <div className="director-about-container">
-        <p>Aniket Sritava has been the strongest pillar on which EdiXual was built on. With his inertly inquisitive mind, he has managed to excel in a wide range of skills. When asked how he manages to do all that, he says<br /><br /><strong>“I’m just trying to create a movie called LIFE.”</strong></p>
-      </div>
-    </div>
-  )
-}
-
-export default EXCOriginalsDirector
->>>>>>> 007c24cc64b54c59379ea99b5f4153d877839866
--- src/components/EXCOriginalsSectionTitle.js
+++ src/components/EXCOriginalsSectionTitle.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react'
 import EXCOriginalsSectionCall from './EXCOriginalsSectionCall'
 
@@ -47,49 +46,3 @@ function EXCOriginalsSectionTitle(props) {
 }
 
 export default EXCOriginalsSectionTitle
-=======
-import React, { useEffect } from 'react'
-import EXCOriginalsSectionCall from './EXCOriginalsSectionCall'
-
-import './EXCOriginalsSectionTitle.css'
-
-function EXCOriginalsSectionTitle(props) {
-  const title = props.path.slice(14)
-  const titles = ["WhoAreWe", "WhatHaveWeDone", "HowToReachUs", "ComingUpNext"]
-  const titleIndex = titles.indexOf(title)
-  const titleInfo = [
-    {
-      "secTitle" : "Who are we?",
-      "secTitleInfo" : "As the name suggests EXC Originals is a place where originality and aesthetics come together with the sole purpose of producing something magical for the viewers."
-    },
-    {
-      "secTitle" : "What have we done?",
-      "secTitleInfo" : "The idea has always been to create pieces of art that add value to the world. We shoot, edit and promote all kinds of filmography projects. Short of creating full length movies, we can cater to any kind of creative aesthetics. May it be photo-shoots, movie stills, or artist portfolios, we do it all."
-    },
-    {
-      "secTitle" : "How to reach us?",
-      "secTitleInfo" : "You are just one phone call or email away from hiring the dream team for your project."
-    },
-    {
-      "secTitle": "Coming up next!",
-      "secTitleInfo": "The EdiXual Creation pipeline has a new exciting project ready for release, watch this space for more updates."
-    }
-  ]
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  })
-
-  return (
-    <>
-    <div className="page-title-container">
-      <h1 className="page-title">{ titleInfo[titleIndex]["secTitle"] }</h1>
-      <p className="page-title-info">{ titleInfo[titleIndex]["secTitleInfo"] }</p>
-    </div>
-    <EXCOriginalsSectionCall title = { title } />
-    </>
-  )
-}
-
-export default EXCOriginalsSectionTitle
->>>>>>> 007c24cc64b54c59379ea99b5f4153d877839866

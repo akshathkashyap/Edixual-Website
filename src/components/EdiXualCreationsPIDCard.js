c3c98d683ccd79ef0ac1115db015ff5accd71418
akshathkashyap <akshath.kp99@gmail.com>
2021-10-05 15:28:00 +0530

    modified PID

--- src/components/EdiXualCreationsPIDCard.js
+++ src/components/EdiXualCreationsPIDCard.js
@@ -5,36 +5,32 @@ import './EdiXualCreationsPIDCard.css'
 function EdiXualCreationsPIDCard(props) {
   const headings = ["List of our Clients", "Graphic designing, Illustrations", "Editing", "GIFs"]
   const headingIndex = headings.indexOf(props.heading)
-  const clientData = [
+  const gdiData = [
     {
-      "title": "client1",
-      "gotInfo": false,
-      "info": "",
-      "link": "Visit Website",
+      "title": "Graphic designing project",
+      "details": "I was a project done for company 1",
+      "link": "View Project",
       "linkTo": "https://www.edixual.com/",
       "imgName": "aboutUs",
     },
     {
-      "title": "client2",
-      "gotInfo": false,
-      "info": "",
-      "link": "Visit Website",
+      "title": "Illustration project",
+      "details": "I was a project done for company 2",
+      "link": "View Project",
       "linkTo": "https://www.edixual.com/",
       "imgName": "aboutUs",
     },
     {
-      "title": "client3",
-      "gotInfo": false,
-      "info": "",
-      "link": "Visit Website",
+      "title": "Illustration project",
+      "details": "I was a project done for company 3",
+      "link": "View Project",
       "linkTo": "https://www.edixual.com/",
       "imgName": "aboutUs",
     },
     {
-      "title": "client4",
-      "gotInfo": false,
-      "info": "",
-      "link": "Visit Website",
+      "title": "Graphic designing project",
+      "details": "I was a project done for company 4",
+      "link": "View Project",
       "linkTo": "https://www.edixual.com/",
       "imgName": "aboutUs",
     },
@@ -43,23 +39,26 @@ function EdiXualCreationsPIDCard(props) {
     console.log(link)
   }
 
-  if (headingIndex === 0) {
+  if (headingIndex === 1) {
     return (
       <div className="pid-card-container">
         {
-          clientData.map((client) => {
+          gdiData.map((gdi) => {
             return (
-              <div key={client.title} className='card'>
+              <div key={gdi.title} className='card'>
+                <div className='card-title'>{gdi.title}</div>
                 <div className='card-image'>
-                  <img src={ process.env.PUBLIC_URL + `/images/${client.imgName}.jpg` } alt="Card Image" />
+                  <img src={ process.env.PUBLIC_URL + `/images/${gdi.imgName}.jpg` } alt="Card Image" />
                 </div>
+                <div className='card-details'>{gdi.details}</div>
                 <div className='card-link'>
-                  <button onClick={() => linkHandler(client.linkTo)}>{client.link}</button>
+                  <button onClick={() => linkHandler(gdi.linkTo)}>{gdi.link}</button>
                 </div>
               </div>
             )
           })
         }
+        <div className='card-container-btn expand'><span></span><span></span></div>
       </div>
     )
   }

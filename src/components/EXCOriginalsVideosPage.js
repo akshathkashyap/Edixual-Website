5723a662442ab8bd91d659ad025e2cb2b4bc824d
akshathkashyap <akshath.kp99@gmail.com>
2021-12-28 04:28:56 +0530

    final videos page

--- src/components/EXCOriginalsVideosPage.js
+++ src/components/EXCOriginalsVideosPage.js
@@ -3,46 +3,60 @@ import React from 'react'
 import './EXCOriginalsVideosPage.css'
 
 function EXCOriginalsVideosPage() {
-    const videosLinks = [
+    const videos = [
         {
-            linkId: 0,
-            linkUrl: "https://www.youtube.com/embed/agoTVmCUh70?autoplay=1&mute=1",
+            title: 'Showreels',
+            url: "https://www.youtube.com/embed/VxQDE5-JgTA?autoplay=1&mute=1",
+            list: [{name: 'Showreel 2019', url: "https://www.youtube.com/watch?v=z4Kjtf2aO6w&t=25s"}],
         },
         {
-            linkId: 1,
-            linkUrl: "https://www.youtube.com/embed/agoTVmCUh70?autoplay=1&mute=1",
+            title: 'Events',
+            url: "https://www.youtube.com/embed/lYc1dOVl55M?autoplay=1&mute=1",
+            list: [{name: 'Ace Tech Exhibition - 2018 Day 2!', url: "https://www.youtube.com/watch?v=JWBARduPMmk"}, {name: 'Ace Tech Exhibition Day 1', url: "https://www.youtube.com/watch?v=8I4AuuG-Jas&t=1s"}, {name: 'Launch of New Range of GM Fans at JITO Udaan', url: "https://www.youtube.com/watch?v=-susUCCDqRw"}, {name: 'Abhinesh Weds Apekshita', url: "https://youtu.be/oup0gJBtVV4"}],
         },
         {
-            linkId: 2,
-            linkUrl: "https://www.youtube.com/embed/agoTVmCUh70?autoplay=1&mute=1",
+            title: 'Brands',
+            url: "https://www.youtube.com/embed/QVoXKg2jMn0?autoplay=1&mute=1",
+            list: [{name: 'ICICI Suna kya case study', url: "https://youtu.be/U2uZ_qOjx40"}, {name: 'syska final render', url: "https://youtu.be/P7g-ilaLyMk"}, {name: 'Meme Compilation #1 | Sajjad Khan', url: "https://youtu.be/GBPCs7k_OCw",}],
         },
         {
-            linkId: 3,
-            linkUrl: "https://www.youtube.com/embed/agoTVmCUh70?autoplay=1&mute=1",
+            title: 'Restaurents and Clubs',
+            url: "https://www.youtube.com/embed/NL6O9qVeuP4?autoplay=1&mute=1",
+            list: [{name: "The Stadium Bar Valentine's day Aftermovie", url: "https://youtu.be/BqOeaX51qmI"}, {name: 'Roti & Grill Menu Overview (EdiXual Creations version)', url: "https://youtu.be/kbeUq9-Fl50"}],
         },
         {
-            linkId: 4,
-            linkUrl: "https://www.youtube.com/embed/agoTVmCUh70?autoplay=1&mute=1",
+            title: 'Vlogs',
+            url: "https://www.youtube.com/embed/kmpU6jbUrlc?autoplay=1&mute=1",
+            list: [{name: 'Mohsin Khan’s Birthday in Singapore | Halloween 2019', url: "https://youtu.be/cwvM3b4enzA"}, {name: '2 WEDDINGS IN 1 DAY! Mumbai & Surat |', url: "https://www.youtube.com/watch?v=JKUC9BhscrI&t=428s"}, {name: 'How to Style Joggers | Simple & Easy Ways', url: "https://www.youtube.com/watch?v=Hcj157xfb00&t"}, {name: 'WHEN YOU LOVE YOUR CAR TOO MUCH', url: "https://youtu.be/O6XjbIOwXOI"}],
         },
         {
-            linkId: 5,
-            linkUrl: "https://www.youtube.com/embed/agoTVmCUh70?autoplay=1&mute=1",
+            title: 'Personal projects',
+            url: "https://www.youtube.com/embed/Vuq4fgjAJhM?autoplay=1&mute=1",
+            list: [{name: 'MNM Collaring Drive with #TeamEXC', url: "https://youtu.be/wobPZMynFb4"}, {name: 'Project Funny News', url: "https://youtu.be/gZfDtDBXk5Y"}, {name: 'Office Inauguration | EdiXual Creations & Dynamic Events', url: "https://youtu.be/4bKtBhU2anU"}],
         },
 
     ]
     return (
         <div className="videos-page section-container">
         <h1 className="section-title">videos</h1>
-            <div className="videos-page-frames-container">
-                {videosLinks.map((link) => {
+        <div className="videos-page-frames-container">
+            {videos.map((video) => {
                 return (
-                    <div key={`${link.linkId}`} className="videos-page-frame">
-                        <iframe src={`${link.linkUrl}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <div key={`${video.title}`} className="videos-page-frame">
+                        <header className='video-title'>{video.title}</header>
+                        <iframe src={`${video.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <ul>
+                            {video.list.map((listItem) => {
+                                return (
+                                    <li onClick={() => {window.open(listItem.url)}}>{listItem.name}</li>
+                                )
+                            })}
+                        </ul>
                     </div>
                 )
-                })}
-            </div>
+            })}
         </div>
+    </div>
     )
 }
 
